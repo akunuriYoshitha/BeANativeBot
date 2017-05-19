a35825ca46788933cbd11500402fe5a8572be100
akunuriYoshitha <ayoshitha1997@gmail.com>
2017-05-19 12:07:53 +0530

    Add project files.

---
+++ BeANativeBot/LuisDialogs.cs
@@ -0,0 +1,112 @@
+﻿using Be_A_Native;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BeANativeBot
+{
+    [LuisModel("ab168c9f-63a9-4b78-a542-abcf1d5e6704", "6b7d416d2dd8485fa78567299ada848a")]
+    [Serializable]
+    public class LuisDialogs : LuisDialog<object>
+    {
+        public string msg1;
+        public void LuisDialogs_initialize(string msg2)
+        {
+            msg1 = msg2;
+            return;
+        }
+        [LuisIntent("Greetings")]
+        public async Task Greetings(IDialogContext context, LuisResult result)
+        {
+            PromptDialog.Confirm(context, ConfirmAboutPrompt, $"" + msg1 + Environment.NewLine + 
+                Environment.NewLine + "Do you want to know something about me?");
+
+        }
+
+        public async Task ConfirmAboutPrompt(IDialogContext context, IAwaitable<bool> result)
+        {
+            if (await result)
+            {
+                await context.PostAsync($"I help you find the most attraction places around you." + Environment.NewLine + Environment.NewLine +
+                    "I love to learn your interests and suggest places accordingly");
+            }
+            else
+            {
+                await context.PostAsync($"Hope you already heard about me" + Environment.NewLine + Environment.NewLine +
+                    "If not, you can know about me any time when you are free");
+            }
+        }
+
+        [LuisIntent("About")]
+        public async Task About(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"I help you find the most attraction places around you." + Environment.NewLine + Environment.NewLine + "I love to learn your interests and suggest places accordingly");
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("AskPlaces")]
+        public async Task AskPlaces(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Searching....");
+            string placename = "";
+            EntityRecommendation rec;
+            if (result.TryFindEntity("place", out rec))
+            {
+                placename = rec.Entity;
+                RestClient rs = new RestClient();
+                string http_response = rs.MakeRequest(placename);
+                await context.PostAsync($"" + http_response);
+            }
+            else
+            {
+                await context.PostAsync($"Could not find place....");
+            }
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("AskDetails")]
+        public async Task AskDetails(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Details are as follows....");
+        }
+        [LuisIntent("Response")]
+        public async Task UserAcceptance(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Hope you will enjoy atleast few of them...");
+        }
+        /*
+        private async Task ConfirmAboutPrompt(IDialogContext context, IAwaitable<bool> result)
+        {
+            await context.PostAsync($"in confirm about prompt");
+            if (await result)
+            {
+                await context.PostAsync($"I help you find the most attraction places around you." + Environment.NewLine + Environment.NewLine +
+                    "I love to learn your interests and suggest places accordingly");
+            }
+            else
+            {
+                await context.PostAsync($"Hope you already heard about me" + Environment.NewLine + Environment.NewLine +
+                    "If not, you can know about me any time when you are free");
+            }
+        }*/
+
+        [LuisIntent("EndConversation")]
+        public async Task EndConversation(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Bye...." + Environment.NewLine + Environment.NewLine +
+                "Please visit again" + Environment.NewLine + Environment.NewLine + 
+                "Hope you have a nice day..." + Environment.NewLine);
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Could not get you... Please try again...");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ BeANativeBot/RestClient.cs
@@ -0,0 +1,207 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+
+public enum HttpVerb//types of request methods.choode one from these to make request
+{
+    GET,
+    POST,
+    PUT,
+    DELETE
+}
+
+namespace Be_A_Native
+{
+    //all classes from the json file. see the response from api
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Geometry
+    {
+        public Location location { get; set; }
+    }
+
+    public class OpeningHours
+    {
+        public bool open_now { get; set; }
+    }
+
+    public class Photo
+    {
+        public int height { get; set; }
+        public List<object> html_attributions { get; set; }
+        public string photo_reference { get; set; }
+        public int width { get; set; }
+    }
+
+    public class AltId
+    {
+        public string place_id { get; set; }
+        public string scope { get; set; }
+    }
+
+    public class Result
+    {
+        public Geometry geometry { get; set; }
+        public string icon { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public OpeningHours opening_hours { get; set; }
+        public List<Photo> photos { get; set; }
+        public string place_id { get; set; }
+        public string scope { get; set; }
+        public List<AltId> alt_ids { get; set; }
+        public string reference { get; set; }
+        public List<string> types { get; set; }
+        public string vicinity { get; set; }
+    }
+
+    public class RootObject//root of places api places search
+    {
+        public List<object> html_attributions { get; set; }
+        public List<Result> results { get; set; }
+        public string status { get; set; }
+    }
+
+    public class AddressComponent
+    {
+        public string long_name { get; set; }
+        public string short_name { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class Northeast
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Southwest
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+
+    public class Result_city
+    {
+        public List<AddressComponent> address_components { get; set; }
+        public string formatted_address { get; set; }
+        public Geometry geometry { get; set; }
+        public string place_id { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class RootObject_city//root object of city lat long extraction
+    {
+        public List<Result> results { get; set; }
+        public string status { get; set; }
+    }
+
+    public class RestClient//main class starts here
+    {
+        public string EndPoint { get; set; }//final uri of places search api
+        public string citypoint { get; set; }//final uri of city latlong api
+        public HttpVerb Method { get; set; }
+        public string ContentType { get; set; }
+        public string PostData { get; set; }
+
+        public RestClient()//constructor for initialization
+        {
+            Method = HttpVerb.GET;
+            ContentType = "text/xml";
+            PostData = "";
+        }
+
+        public string MakeRequest(String cityname)//making two requests 1. lalng extraction 2.places search based on latlong
+        {
+            Double latitude;
+            Double longitude;
+            if (true)//to make differentiation btwn two requests
+            {
+                citypoint = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityname + "&key=AIzaSyANeYnxWuP4GV-GZtPu1wkOmNIxJmLn2sg";//city api request uri
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(citypoint);//making a http web request
+                request.Method = Method.ToString();//assigning method to request
+
+                using (HttpWebResponse res = (HttpWebResponse)request.GetResponse())//using means using internal method getting method response and assigning
+                {
+                    if (res.StatusCode != HttpStatusCode.OK)//checking error code
+                    {
+                        return "error " + res.StatusCode.ToString();
+                    }
+                    Stream res_stream = res.GetResponseStream();//creating a response stream to read the response
+
+                    StreamReader st_read = new StreamReader(res_stream);//crreating a stream reader to read from the response stream
+                    string city_json = st_read.ReadToEnd();//reading the response stream until the end and assigning to string
+                    //the response we get is in the form of a json but converted to string when we get, this is serialization
+                    //once we get the string form of data we convert it into jason to access internal class objects and data,this is called deserialization
+                    RootObject_city city_obj = JsonConvert.DeserializeObject<RootObject_city>(city_json);//Root object of city deserialization of json data
+
+                    latitude = city_obj.results[0].geometry.location.lat;//extraction of latitude from city object root obj -> results[0] -> geometry ->location -> lat see response
+                    longitude = city_obj.results[0].geometry.location.lng;
+
+                }
+            }
+            String res_str = "";
+            String res_final = "";
+            List<string> Types = new List<string> { };//types of data we want to extract from places types supported are given in place types website
+            Types.Add("museum");
+            Types.Add("zoo");
+            Types.Add("hindu_temple");
+            Types.Add("shopping_mall");
+            Types.Add("church");
+            Types.Add("art_gallery");
+            foreach (string type in Types)//make one request for each type 
+            {
+
+                EndPoint = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "location=" + latitude + "," + longitude + "&radius=500&type=" + type + "&key=AIzaSyDyjc_fG7ZIBbJcE4EoTZKMDKDFENLSwdg";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(EndPoint);
+                request.Method = Method.ToString();
+                using (HttpWebResponse res = (HttpWebResponse)request.GetResponse())
+                {
+                    if (res.StatusCode != HttpStatusCode.OK)
+                    {
+                        return "error " + res.StatusCode.ToString();
+                    }
+                    Stream res_stream = res.GetResponseStream();
+
+                    StreamReader st_read = new StreamReader(res_stream);
+                    res_str = st_read.ReadToEnd();
+
+                    RootObject r = JsonConvert.DeserializeObject<RootObject>(res_str);//google places are already sorted in the order of popularity based on some constraints
+                    int flag = 0;
+                    foreach (Result o in r.results)//from the results we extract places and add to the response crating a formed output
+                    {
+                        if (flag == 0)//printing type of the place once 
+                        {
+                            res_final += (type.ToUpper() + Environment.NewLine + Environment.NewLine);
+                        }
+                        res_final += (o.name + Environment.NewLine + Environment.NewLine);
+                        flag++;
+
+                        if (flag == 5)//maxium or top 5 popular places are retrived
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return res_final;//final output or response code
+        }
+
+    } // class
+}//name space
